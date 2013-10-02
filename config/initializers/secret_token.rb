6add98b84ecf4d1fcec7ba5545c19cb69a55007a
Nicholas P Smith <npsmith1990@gmail.com>
2013-10-01 23:27:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySocialHub::Application.config.secret_key_base = '499e3cef604a3c630099c0987a5bb5aaf70d2c363674af5ecddd00401e4f1979cc8a983d6d277c16242b4082d6aee45cb5f3d8cb5ab955e80400ad19596c6a14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MySocialHub::Application.config.session_store :cookie_store, key: '_MySocialHub_session'
